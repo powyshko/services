38d42e781eccae6531ea9787b23214a58db48c93
powyshko <povyshko.y@gmail.com>
2021-04-06 21:35:30 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,125 +1,130 @@
-document.addEventListener('DOMContentLoaded', e => {
+document.addEventListener("DOMContentLoaded", (e) => {
+  const linkActive = document.querySelectorAll(".active-link");
+  const burger = document.querySelector(".burger");
+  const tabNav = document.querySelectorAll(".tab-nav");
+  const tabContent = document.querySelectorAll(".tab-content");
+  const scrollContainer = document.querySelectorAll(".scroll-container");
+  const scrollLinks = document.querySelectorAll(".menu-services__item");
 
-  const linkActive = document.querySelectorAll('.active-link');
-  const burger = document.querySelector('.burger');
-  const tabNav = document.querySelectorAll('.tab-nav');
-  const tabContent = document.querySelectorAll('.tab-content');
-  const scrollContainer = document.querySelectorAll('.scroll-container');
-  const scrollLinks = document.querySelectorAll('.menu-services__item');
+  const garanteeQuestionsSubtitleAnimate = document.querySelectorAll(
+    ".garantee__questions-subtitle"
+  );
+  const garanteeQuestionsBodyAnimate = document.querySelectorAll(
+    ".garantee__questions-body"
+  );
+  const indexParagraphyAnimate = document.querySelectorAll(
+    ".services-column__paragraphy-animate"
+  );
+  const indexServicesBlockAnimate = document.querySelectorAll(
+    ".services-column__block"
+  );
+  const awardsAnimateIMG = document.querySelectorAll(".awards__img");
 
-  const garanteeQuestionsSubtitleAnimate = document.querySelectorAll('.garantee__questions-subtitle');
-  const garanteeQuestionsBodyAnimate = document.querySelectorAll('.garantee__questions-body');
-  const indexParagraphyAnimate = document.querySelectorAll('.services-column__paragraphy-animate');
-  const indexServicesBlockAnimate = document.querySelectorAll('.services-column__block');
-  const awardsAnimateIMG = document.querySelectorAll('.awards__img');
+  //Анимация при клике на якорь
 
+  for (let i = 0; i < scrollLinks.length; i++) {
+    scrollLinks[i].addEventListener("click", (e) => {
+      e.preventDefault();
 
-//Анимация при клике на якорь
+      const id = e.target.getAttribute("href");
 
-for(let i = 0; i < scrollLinks.length; i++){
-  scrollLinks[i].addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const id = e.target.getAttribute('href');
-
-    document.querySelector(id).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     });
-  })
-}
+  }
 
   //Анимация при вертикальном скроле
   ScrollReveal().reveal(garanteeQuestionsSubtitleAnimate, {
-    distance: '0px',
+    distance: "0px",
     opacity: 0.1,
     duration: 1000,
     mobile: false,
-  })
+  });
 
   ScrollReveal().reveal(garanteeQuestionsBodyAnimate, {
-    distance: '100px',
+    distance: "100px",
     duration: 1000,
     mobile: false,
-  })
+  });
 
   ScrollReveal().reveal(indexParagraphyAnimate, {
-    distance: '100px',
+    distance: "100px",
     duration: 1000,
     viewFactor: 0.5,
     mobile: false,
-  })
+  });
 
   ScrollReveal().reveal(indexServicesBlockAnimate, {
-    distance: '100px',
+    distance: "100px",
     duration: 1000,
     viewFactor: 0.2,
     mobile: false,
-  })
+  });
 
   ScrollReveal().reveal(awardsAnimateIMG, {
     scale: 0.5,
     duration: 1000,
     viewFactor: 0.3,
     mobile: false,
-  })
+  });
 
-  if($("#sidebar").length) {
-    $('#sidebar').stickySidebar({
-      containerSelector: 'main-project__body',
-      innerWrapperClass: 'menu-services__body',
+  if ($("#sidebar").length) {
+    $("#sidebar").stickySidebar({
+      containerSelector: "main-project__body",
+      innerWrapperClass: "menu-services__body",
       topSpacing: 50,
-      bottomSpacing: 0
+      bottomSpacing: 0,
     });
   }
 
-  burger.addEventListener('click', onActiveMobileMenu);
+  burger.addEventListener("click", onActiveMobileMenu);
 
   activeLink();
 
   scrollHorizontally();
 
-  function activeLink(){
+  function activeLink() {
     let locationPath = location.pathname;
-    linkActive.forEach(link => {
-      let href = link.getAttribute('href');
-      if(locationPath === href){
-        link.classList.add('active')
+    linkActive.forEach((link) => {
+      let href = link.getAttribute("href");
+      if (locationPath === href) {
+        link.classList.add("active");
       }
     });
   }
 
-//Burger
+  //Burger
 
-  function onActiveMobileMenu (){
-    document.querySelector('.burger').classList.toggle("active");
-    document.querySelector('.header__menu').classList.toggle("active");
-    document.querySelector('body').classList.toggle("lock");
+  function onActiveMobileMenu() {
+    document.querySelector(".burger").classList.toggle("active");
+    document.querySelector(".header__menu").classList.toggle("active");
+    document.querySelector("body").classList.toggle("lock");
   }
-
 
   function scrollHorizontally(e) {
     e = window.event || e;
-    let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    let delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
 
-    if(scrollContainer.length > 0){
-      scrollContainer.forEach(item => {
-        item.scrollLeft -= (delta*40); // Multiplied by 40
+    if (scrollContainer.length > 0) {
+      scrollContainer.forEach((item) => {
+        item.scrollLeft -= delta * 40; // Multiplied by 40
 
-        if(item.addEventListener){
+        if (item.addEventListener) {
           item.addEventListener("mousewheel", scrollHorizontally, false);
           item.addEventListener("DOMMouseScroll", scrollHorizontally, false);
-        }else{
+        } else {
           item.attachEvent("onmousewheel", scrollHorizontally);
         }
-      })
+      });
     }
     e.preventDefault();
   }
 
-// Слайдер
+  // Слайдер
 
-$('.jobs-rev__questions-container').slick({
+  $(".jobs-rev__questions-container").slick({
     infinite: true,
     adaptiveHeight: true,
     nextArrow: `<div class="jobs-rev__questions-next">
@@ -130,29 +135,31 @@ $('.jobs-rev__questions-container').slick({
     responsive: [
       {
         breakpoint: 575,
-        settings: 'unslick',
-      }
-    ]
-  })
+        settings: "unslick",
+      },
+    ],
+  });
 
-  $(window).resize(function(){
+  $(window).resize(function () {
     let $windowWidth = $(window).width();
     if ($windowWidth > 575) {
-      $('.jobs-rev__questions-container').not('.slick-initialized').slick({
-        infinite: true,
-        adaptiveHeight: true,
-        nextArrow: `<div class="jobs-rev__questions-next">
+      $(".jobs-rev__questions-container")
+        .not(".slick-initialized")
+        .slick({
+          infinite: true,
+          adaptiveHeight: true,
+          nextArrow: `<div class="jobs-rev__questions-next">
                       <a href="">Другой отзыв</a>
                     </div>`,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [
-          {
-            breakpoint: 576,
-            settings: 'unslick',
-          }
-        ]
-      })
+          autoplay: true,
+          autoplaySpeed: 5000,
+          responsive: [
+            {
+              breakpoint: 576,
+              settings: "unslick",
+            },
+          ],
+        });
     }
   });
 
@@ -162,80 +169,78 @@ $('.jobs-rev__questions-container').slick({
 
   //Табы
 
-  if(tabNav.length > 0){
-    for(let i = 0; i < tabNav.length; i++){
-      tabNav[i].addEventListener('click', e => {
+  if (tabNav.length > 0) {
+    for (let i = 0; i < tabNav.length; i++) {
+      tabNav[i].addEventListener("click", (e) => {
         e.preventDefault();
 
         let tabNavAttr = e.currentTarget.dataset.tab;
 
-        for(let j = 0; j < tabNav.length; j++){
+        for (let j = 0; j < tabNav.length; j++) {
           let contentTabAttr = tabContent[j].dataset.tabContent;
 
-          if(tabNavAttr === contentTabAttr){
-            tabNav[j].classList.add('active');
-            tabContent[j].classList.add('active');
-          }else{
-            tabNav[j].classList.remove('active');
-            tabContent[j].classList.remove('active');
+          if (tabNavAttr === contentTabAttr) {
+            tabNav[j].classList.add("active");
+            tabContent[j].classList.add("active");
+          } else {
+            tabNav[j].classList.remove("active");
+            tabContent[j].classList.remove("active");
           }
         }
-      })
+      });
     }
   }
 
   // Yandex maps
 
-    ymaps.ready(init);
+  // ymaps.ready(init);
 
-  function init(){
-
-    let navElements = document.querySelectorAll('.tab-nav');
-    let coordsElements = document.querySelectorAll('[data-coords]');
+  function init() {
+    let navElements = document.querySelectorAll(".tab-nav");
+    let coordsElements = document.querySelectorAll("[data-coords]");
     let coords = [];
 
-    coordsElements.forEach(el => {
-      let coordsElementsAttr = el.dataset.coords.split(',').map(Number);
+    coordsElements.forEach((el) => {
+      let coordsElementsAttr = el.dataset.coords.split(",").map(Number);
       coords.push(coordsElementsAttr);
-    })
+    });
 
     var myMap = new ymaps.Map("map", {
-      center: [56.83, 60.60],
-      controls: ['zoomControl'],
+      center: [56.83, 60.6],
+      controls: ["zoomControl"],
       zoom: 11,
     });
 
     //Создание меток
 
-    for(let i = 0; i < coords.length; i++){
+    for (let i = 0; i < coords.length; i++) {
       myMap.geoObjects.add(new ymaps.Placemark(coords[i]));
     }
 
     // Перемещение по меткам
 
-    function clickGoTo(coords){
+    function clickGoTo(coords) {
       myMap.panTo(coords, {
         flying: true,
-      })
+      });
 
       return false;
     }
 
-    for (let i = 0; i < navElements.length; i++){
-      navElements[i].addEventListener('click', (e) => {
+    for (let i = 0; i < navElements.length; i++) {
+      navElements[i].addEventListener("click", (e) => {
         e.preventDefault();
         let coords = e.currentTarget.dataset.coords;
-        clickGoTo(coords.split(',').map(Number));
-      })
+        clickGoTo(coords.split(",").map(Number));
+      });
     }
-
   }
 
   // Горизонтальный скролл
-  if($(".reviews__body").length) {
-    var sly = new Sly($('.reviews__body'), {
+  if ($(".reviews__body").length) {
+    var sly = new Sly($(".reviews__body"), {
       horizontal: 1,
-      itemNav: 'basic',
+      itemNav: "basic",
       mouseDragging: 1,
       touchDragging: 1,
       startAt: 0,
@@ -245,4 +250,47 @@ $('.jobs-rev__questions-container').slick({
     sly.init();
   }
 
-})
+  //Анимация меню miltibrand.html
+
+  let count = 1;
+  let animationStop = false;
+
+  function showMenuItem(id) {
+    let countID = id - 1;
+    $(".multibrand__menu-item").each((item) => {
+      if (item === countID) {
+        $(".multibrand__menu-item:eq(" + countID + ")").addClass("active");
+      }
+      $(".multibrand__menu-item")
+        .not(".multibrand__menu-item:eq(" + countID + ")")
+        .removeClass("active");
+    });
+
+    $(".multibrand__menu-progress_bg")[countID].animate(
+      { width: "100%" },
+      5000,
+      function () {
+        $($(".multibrand__menu-progress_bg")[countID]).animate({ width: "0%" });
+      }
+    );
+  }
+  function nextMenuItem(start) {
+    if (!start) count++;
+    if (count > $(".multibrand__menu-progress_bg").length) count = 1;
+    if (!animationStop) {
+      showMenuItem(count);
+      timeoutID = setTimeout(function () {
+        nextMenuItem(0);
+      }, 5000);
+    }
+  }
+
+  $(".multibrand__menu-item").click(function () {
+    animationStop = true;
+    $(".multibrand__menu-progress_bg").css({ opacity: "0" });
+    $(".multibrand__menu-item").removeClass("active active-progress-bg");
+    $(this).addClass("active active-progress-bg");
+  });
+
+  nextMenuItem(1);
+});
